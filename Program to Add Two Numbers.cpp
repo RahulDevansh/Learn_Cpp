562fa97ac95822af08dbb7174929740fdcbe89a5
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2023-07-10 01:12:03 +0530

    Update and rename Program to Add Two Numbers to Program to Add Two Numbers.cpp

--- Program to Add Two Numbers.cpp
+++ Program to Add Two Numbers.cpp
@@ -1,16 +1,13 @@
 // C++ Program to Add Two Numbers 
 
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
     int num1,num2;
     cout<<"Hey please two Numbers"<<endl;
-   
     cin>>num1>>num2;
     cout<< "The addition of two number is : "<<num1+num2;
-
     return 0;
 }
